81525185dbe44a3f727049241a6d1b9385426a07
muldaeng <alstnwkd990@naver.com>
2023-02-04 16:12:23 +0900

    2-1 재귀

--- 2-1 Factorial.c
+++ 2-1 Factorial.c
@@ -2,10 +2,17 @@
 
 int Factorial(int n) 
 {
-
+	if (n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);
 }
 
 int main(void)
 {
+	printf("%d \n", Factorial(10));
+	printf("%d \n", Factorial(9));
+	printf("%d \n", Factorial(4));
+
 	return 0;
 }
